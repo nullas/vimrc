7518749003d98f56492eed1871a846b17952f2a4
nullas <sean.null@gmail.com>
2011-01-19 04:09:55 +0800

    master git

---
+++ hw.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hw.c
+ *
+ *    Description:  For Git
+ *
+ *        Version:  1.0
+ *        Created:  01/19/2011 03:16:57 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Xin Zhao (), sean.null@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+int main(){
+	printf("%s","hello,world!");
+	return 0;
+}
